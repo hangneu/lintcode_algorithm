5290fa422f867a2da596c49f9773c444a22a4b47
hangneu <shangneu1992@gmail.com>
2017-08-21 15:59:57 -0400

    Update coin one line.py

--- coin one line.py
+++ coin one line.py
@@ -1,12 +1,15 @@
-def test(n):
-	if n == 0:
-		return False
-	if n == 1 or n == 2:
-		return True
-	res = [False] * (n + 1)
-	res[1] = True
-	res[2] = True
-	for i in range(3, n + 1):
-		res[i] = res[i-1] == False or res[i-2] == False
-	return res[-1]
-print test(4)
+class Solution:
+    # @param n: an integer
+    # @return: a boolean which equals to True if the first player will win
+    def firstWillWin(self, n):
+        # write your code here
+        if n == 0:
+            return False
+        if n == 1 or n == 2:
+            return True
+        res = [False] * (n + 1)
+        res[1] = True
+        res[2] = True
+        for i in range(3, n + 1):
+		res[i] = (res[i-1] == False) or (res[i-2] == False)
+        return res[-1]
